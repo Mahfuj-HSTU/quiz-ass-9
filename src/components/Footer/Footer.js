78d3b05133ceee81a7a0ff9d884a15c9d752a1f7
Mahfuj-HSTU <rmahfujur16@gmail.com>
2022-10-11 20:50:51 +0600

    footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-slate-800 text-white py-9 rounded-lg'>
+            <h3 className='sm:text-3xl text-2xl'>&copy; Mahfujur Rahman</h3>
+            <p className='sm:text-xl mt-2 text-base '>Frontend Developer</p>
+        </div>
+    );
+};
+
+export default Footer;
--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 
 const Main = () => {
     return (
-        <div className='container mx-auto max-w-[85%] p-2 pt-0'>
+        <div className='container mx-auto max-w-[85%] p-2 pt-0 pb-0'>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
